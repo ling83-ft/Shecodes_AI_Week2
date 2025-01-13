c6bb6dc27e320dc8e4549bba8d18d8a16cfe651e
0-ftan <feiling.tandiono@dfo.no>
2025-01-13 11:17:19 +0100

    add-js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateCookBook(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: "Test recipe",
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
 
+let cookFormElement = document.querySelector("#cook-book-generator");
+cookFormElement.addEventListener("submit", generateCookBook);
